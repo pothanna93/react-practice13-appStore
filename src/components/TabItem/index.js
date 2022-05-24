19e7287487b84e40f249241d5ffb0b9d2fe894e9
pothanna <pothanna.pullolla@gmail.com>
2022-05-24 16:12:43 +0000

    react practice13 appstore

---
+++ src/components/TabItem/index.js
@@ -0,0 +1,25 @@
+import './index.css'
+
+const TabItem = props => {
+  const {tabDetails, updateApps, isActive} = props
+  const {displayText, tabId} = tabDetails
+
+  const onClickTab = () => {
+    updateApps(tabId)
+  }
+
+  const className = isActive ? 'apply' : 'not-apply'
+
+  return (
+    <li className="tab-items">
+      <button
+        type="button"
+        className={`tab-button ${className}`}
+        onClick={onClickTab}
+      >
+        {displayText}
+      </button>
+    </li>
+  )
+}
+export default TabItem
